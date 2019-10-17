83eabcf1e16b892f6849b9a2a6718ea44c890435
Laboratório de Redes <labredes@Labredes.pucrs.br>
2019-10-17 09:44:56 -0300

    nova mudanca

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -7,3 +7,5 @@ BUILDROOT_DIR=$BASE_DIR/..
 COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
 $COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
 $COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test2 $BUILDROOT_DIR/custom-scripts/syscall_test2.c
+cp $BASE_DIR/../custom-scripts/fstab $BASE_DIR/target/etc/fstab
+
